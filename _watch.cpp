bfb69292ae1476546ca31e0b209267583a74c5e5
chhormratana <chhormratana@gmail.com>
2022-07-20 18:51:20 +0700

    20.07.22: _watch

--- _watch.cpp
+++ _watch.cpp
@@ -6,13 +6,22 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    for (int i = 0; i < argc; ++i)
-    {
-        cout << argv[i]<<endl;
-    }
-    string currentPath = get_current_dir() + "/4_exabanque.log";
+    // for (int i = 0; i < argc; ++i)
+    // {
+    //     cout << argv[i]<<endl;
+    // }
+    string currentPath = get_current_dir();
 
-    cout << currentPath << endl;
+    replace(currentPath, WC_PATH, "");
+
+    if (currentPath.find("/") != std::string::npos)
+    {
+        cout << "Invalid project path: " << get_current_dir() << endl;
+    }
+
+    string logPath = string_format((WC_PATH + currentPath + LOG_PATH), 4);
+    cout << logPath;
+
     return 0;
 }
 
